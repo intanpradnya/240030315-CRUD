5b1d7921f48fbec3c4a4418903cd613207d494fe
intanpradnya <cmiwintan@gmail.com>
2025-11-29 21:31:59 +0800

    Menambahkan bootstrap dan fontawesome

---
+++ kelola.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <!-- Bootstrap --> 
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="js/bootstrap.bundle.min.js" ></script>
+    <!-- Font Awesome --> 
+	<link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+	<title>uts_crud</title>
+</head>
+
+
+
+
+
+
+</html>
