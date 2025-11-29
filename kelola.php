f4096da0b8a63a211507887a704bab226a011525
intanpradnya <cmiwintan@gmail.com>
2025-11-29 21:33:27 +0800

    Menambahkan navbar

--- kelola.php
+++ kelola.php
@@ -9,6 +9,10 @@
 	<link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
 	<title>uts_crud</title>
 </head>
+<body>
+	<nav class="navbar navbar-light bg-light mb-4">
+  		<div class="container-fluid">
+    		<a class="navbar-brand" href="#">
 
 
 
