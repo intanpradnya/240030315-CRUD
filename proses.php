763bef8519ea9d320b799d58d6a5896f63078d33
intanpradnya <cmiwintan@gmail.com>
2025-11-29 22:03:25 +0800

    Menambahkan query dan sql

--- proses.php
+++ proses.php
@@ -8,10 +8,14 @@
 			$jenisKelamin = $_POST['jenis_kelamin'];
 			$foto = $_FILES['foto']['name'];
 			$alamat = $_POST['alamat'];
-            
+
             $dir = "img/"; 
 			$tmpFile = $_FILES['foto']['tmp_name']; 
 
 			move_uploaded_file($tmpFile, $dir.$foto); 
 
 			//die(); 
+
+            
+            $query = "INSERT INTO tb_mahasiswa VALUES(null, '$nim', '$namaMahasiswa', '$jenisKelamin', '$foto', '$alamat')"; 
+			$sql = mysqli_query($conn, $query); 
