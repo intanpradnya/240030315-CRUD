98488849070c292400f33483c94a476ce78f39e7
intanpradnya <cmiwintan@gmail.com>
2025-11-29 22:07:17 +0800

    Menambahkan hapus, update

--- proses.php
+++ proses.php
@@ -46,3 +46,33 @@
 					unlink("img/".$result['foto_mahasiswa']); 
 					move_uploaded_file($_FILES['foto']['tmp_name'], 'img/'.$_FILES['foto']['name']); 
 				}
+            $query = "UPDATE tb_mahasiswa SET nim='$nim', nama_mahasiswa='$nama_mahasiswa', jenis_kelamin='$jenis_kelamin', alamat='$alamat', foto_mahasiswa='$foto' WHERE id_mahasiswa='$id_mahasiswa';";
+			$sql = mysqli_query($conn, $query); 
+				header("location: index.php"); 
+
+		}
+            }
+		if(isset($_GET['hapus'])){
+			$id_mahasiswa = $_GET['hapus']; 
+
+			$queryShow = "SELECT * FROM tb_mahasiswa WHERE id_mahasiswa = '$id_mahasiswa';";
+			$sqlShow = mysqli_query($conn, $queryShow); 
+			$result = mysqli_fetch_assoc($sqlShow); 
+
+			//var_dump($result); 
+			unlink("img/".$result['foto_mahasiswa']); 
+
+
+			$query = "DELETE FROM tb_mahasiswa WHERE id_mahasiswa = '$id_mahasiswa';";
+			$sql = mysqli_query($conn, $query);
+			
+			if($sql){
+				header("location: index.php"); 
+				//echo "Data Berhasil Ditambahkan <a href='index.php'>[Home]</a>";
+			}else{
+				echo $query; 
+			}
+
+			echo "Hapus Data <a href='index.php'>[Home]</a>"; 
+		}
+?>
