6437751bf271030a00853b1d439f440e2e92f9b4
intanpradnya <cmiwintan@gmail.com>
2025-11-29 22:01:12 +0800

    Menambahkan dir

--- proses.php
+++ proses.php
@@ -8,3 +8,9 @@
 			$jenisKelamin = $_POST['jenis_kelamin'];
 			$foto = $_FILES['foto']['name'];
 			$alamat = $_POST['alamat'];
+            $dir = "img/"; 
+			$tmpFile = $_FILES['foto']['tmp_name']; 
+
+			move_uploaded_file($tmpFile, $dir.$foto); 
+
+			//die(); 
